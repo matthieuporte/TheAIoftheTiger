548dd2d5798f680d4e7521a3ef8de84d08d9bfdf
Matthieu Porte <matthieu.porte@epita.fr>
2025-06-16 11:33:57 +0200

    interval.ml (implem) : all implementation but widen. Added a test.

--- src/domains/interval.ml
+++ src/domains/interval.ml
@@ -19,7 +19,20 @@ let print fmt itv =
 
 (* join *)
 (* Step 4 *)
-let join _ _ = Top
+let join i1 i2 =
+  match (i1, i2) with
+  | Range (low1, high1), Range (low2, high2) ->
+      let low = if low1 < low2 then low1 else low2
+      and high = if high1 > high2 then high1 else high2 in
+      Range (low, high)
+  | Range (low1, high1), Minf up | Minf up, Range (low1, high1) ->
+      let high = if high1 > up then high1 else up in
+      Minf high
+  | Range (low1, high1), Inf low2 | Inf low2, Range (low1, high1) ->
+      let low = if low1 < low2 then low1 else low2 in
+      Inf low
+  | _ -> Top
+
 let widen _ _ = Top
 
 let subset a b =
@@ -40,10 +53,20 @@ let neg (i : t) : t =
   | Minf x -> Inf (-x)
   | Range (l, u) -> Range (-u, -l)
 
-
-
 (* Step 4 *)
-let add _i1 _i2 = Top
+let add (_i1 : t) (_i2 : t) : t =
+  match _i1 with
+  | Range (low1, high1) -> (
+      match _i2 with
+      | Range (low2, high2) -> Range (low1 + low2, high1 + high2)
+      | Minf i -> Minf (i + high1)
+      | Inf i -> Inf (i + low1)
+      | Top -> Top)
+  | Minf i -> (
+      match _i2 with Range (low, high) -> Minf (i + high) | _ -> Top)
+  | Inf i -> ( match _i2 with Range (low, high) -> Inf (i + low) | _ -> Top)
+  | Top -> Top
+
 let sub _i1 _i2 = Top
 let mul _i1 _i2 = Top
 let div _i1 _i2 = Top
@@ -67,13 +90,49 @@ let logical_or a b =
   if subset a false_ && subset b false_ then false_ else maybe_
 
 (* comparisons *)
+let reverse b =
+  let t = truth b in
+  match t with Domain.True -> false_ | Domain.False -> true_ | _ -> maybe_
+
 (* Step 4 *)
-let eq _ _ = maybe_
-let ne _ _ = maybe_
-let gt _ _ = maybe_
-let ge _ _ = maybe_
-let lt _ _ = maybe_
-let le _ _ = maybe_
+let eq i1 i2 =
+  match (i1, i2) with
+  | Range (low1, high1), Range (low2, high2) ->
+      if low1 = low2 && low1 = high1 && low2 = high2 then true_
+      else if low1 > high2 || low2 > high1 then false_
+      else maybe_
+  | Inf low, Minf up | Minf up, Inf low -> if low > up then false_ else maybe_
+  | _, _ -> maybe_
+
+let ne i1 i2 = reverse (eq i1 i2)
+
+(* i1 > i2 *)
+let gt i1 i2 =
+  match i1 with
+  | Range (low1, high1) -> (
+      match i2 with
+      | Range (low2, high2) ->
+          if low1 > high2 then true_
+          else if low2 >= high1 then false_
+          else maybe_
+      | Minf up -> if low1 > up then true_ else maybe_
+      | Inf low -> if high1 < low then false_ else maybe_
+      | _ -> maybe_)
+  | Minf up -> (
+      match i2 with
+      | Range (low2, high2) -> if up <= low2 then false_ else maybe_
+      | Inf low -> if up <= low then false_ else maybe_
+      | _ -> maybe_)
+  | Inf low -> (
+      match i2 with
+      | Range (low2, high2) -> if low > high2 then true_ else maybe_
+      | Minf up -> if up < low then true_ else maybe_
+      | _ -> maybe_)
+  | _ -> maybe_
+
+let ge i1 i2 = join (gt i1 i2) (eq i1 i2)
+let lt i1 i2 = reverse (ge i1 i2)
+let le i1 i2 = reverse (gt i1 i2)
 
 (* constructors *)
 let of_int x = Range (x, x)
